70088fef6a44d5f5ebefe8c42d9200df908da054
Tanat <tjeis4590@gmail.com>
2023-03-16 10:43:11 +0700

    added GUI class and GUI system

--- hello.java
+++ hello.java
@@ -1,10 +1,25 @@
 import java.util.Scanner;
+import javax.swing.*;
 
 class structure {
 
+
+
     public static void main(String args[]) {
-        
+
     }
     
 
+}
+
+
+class gui{
+    public static void main(String args[]){
+       JFrame frame = new JFrame("Voting Program (Testing)");
+       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       frame.setSize(300,300);
+       JButton button = new JButton("Login to your account");
+       frame.getContentPane().add(button); // Adds Button to content pane of frame
+       frame.setVisible(true);
+    }
 }
