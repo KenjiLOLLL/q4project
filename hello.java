5eabedc3f1476aa7c716a78ad63d5fdf7d010fd3
Tanat <tjeis4590@gmail.com>
2023-03-16 10:51:53 +0700

    adjusted buttons

--- hello.java
+++ hello.java
@@ -18,7 +18,7 @@ class gui{
       public static void main(String args[]){
            JFrame frame = new JFrame("My First GUI");
            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-           frame.setSize(1000,1000);
+           frame.setSize(300,300);
           JButton button1 = new JButton("Button 1");
           JButton button2 = new JButton("Button 2");
           frame.getContentPane().add(button1);
